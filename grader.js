3566e217ea3c2c81ec1079f8ad866696e9a6d425
Carlos Ferro <carlosferro@gmail.com>
2013-07-14 00:30:02 +0000

    Updating grader.js to receive an URL as an option

--- grader.js
+++ grader.js
@@ -26,6 +26,17 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://safe-taiga-1501.herokuapp.com";
+var rest = require('restler');
+
+var assertURLExists = function(url) {
+    var instr = url.toString();
+    if(instr == "") {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -42,6 +53,17 @@ var cheerioHtmlFile = function(htmlfile) {
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlURL = function(htmlurl, checksfile) {
+    $ = rest.parser(htmlurl); 
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
@@ -65,8 +87,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url <html_file>', 'URL to index.html', clone(assertURLExists), URL_DEFAULT)
+	.parse(process.argv);
+    var checkJson;
+    if (program.file) {
+	checkJson = checkHtmlFile(program.file, program.checks);
+    } else if (program.url) {
+	checkJson = checkHtmlURL(program.url, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
